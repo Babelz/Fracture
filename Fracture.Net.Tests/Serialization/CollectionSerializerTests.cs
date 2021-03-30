f2d2e757a0a9c0eba05a78ebf353a41c11418933
Niko <nikoantsal@gmail.com>
2021-03-30 19:58:24 +0300

    added collection serializer tests

---
+++ Fracture.Net.Tests/Serialization/CollectionSerializerTests.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+namespace Fracture.Net.Tests.Serialization
+{
+    [Trait("Category", "Serialization")]
+    public sealed class CollectionSerializerTests
+    {
+        
+    }
+}
