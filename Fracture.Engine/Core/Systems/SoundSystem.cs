34baa6182dfcd828c913c36e3960e24374b537a8
Niko <nikoantsal@gmail.com>
2022-05-16 23:42:25 +0300

    add basic sound systems

---
+++ Fracture.Engine/Core/Systems/SoundSystem.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fracture.Common.Di.Attributes;
+using Fracture.Common.Events;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace Fracture.Engine.Core.Systems
+{
+    public readonly struct VolumeChangedEventArgs : IStructEventArgs
+    {
+        #region Properties
+        public string ChannelName
+        {
+            get;
+        }
+
+        public float Value
+        {
+            get;
+        }
+        #endregion
+
+        public VolumeChangedEventArgs(string channelName, float value)
+        {
+            ChannelName = channelName;
+            Value       = value;
+        }
+    }
+    
+    public static class DefaultSoundChannels
+    {
+        #region Constant fields
+        public const string Master = "master";
+        
+        public const string Music = "music";
+        
+        public const string Effect = "effect";
+        #endregion
+    }
+    
+    public interface ISoundSystem : IGameEngineSystem
+    {
+        #region Events
+        event StructEventHandler<VolumeChangedEventArgs> VolumeChanged; 
+        #endregion
+        
+        void CreateChannel(string name);
+        
+        void SetVolume(string channelName, float value);
+        float GetVolume(string channelName);
+        
+        void Play(SoundEffect effect);
+        bool IsPlaying();
+        
+        void Stop();
+    }
+    
+    public sealed class SoundSystem : GameEngineSystem, ISoundSystem
+    {
+        #region Fields
+        private readonly Dictionary<string, float> channels;
+        
+        private SoundEffectInstance current;
+        #endregion
+        
+        #region Events
+        public event StructEventHandler<VolumeChangedEventArgs> VolumeChanged;
+        #endregion
+
+        [BindingConstructor]
+        public SoundSystem()
+        {
+            channels = new Dictionary<string, float>();
+            
+            CreateChannel(DefaultSoundChannels.Master);
+            CreateChannel(DefaultSoundChannels.Effect);
+            CreateChannel(DefaultSoundChannels.Music);
+        }
+
+        public void CreateChannel(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException(nameof(name));
+            
+            if (!channels.ContainsKey(name))
+                throw new InvalidOperationException($"channel with name {name} already exists");
+            
+            channels.Add(name, default);
+        }
+
+        public void SetVolume(string channelName, float value)
+        {
+            channels[channelName] = value;
+            
+            VolumeChanged?.Invoke(this, new VolumeChangedEventArgs(channelName, value));
+
+            if ((channelName != DefaultSoundChannels.Master && channelName != DefaultSoundChannels.Music) && !IsPlaying()) 
+                return;
+
+            current.Stop();
+                
+            current.Volume = channels[DefaultSoundChannels.Master] * channels[DefaultSoundChannels.Music];
+                
+            current.Resume();
+        }
+        public float GetVolume(string channelName)
+            => channelName == DefaultSoundChannels.Master ? channels[channelName] : channels[channelName] * channels[DefaultSoundChannels.Master];
+
+        public void Play(SoundEffect effect)
+        {
+            current?.Dispose();
+
+            current        = effect.CreateInstance();
+            current.Volume = channels[DefaultSoundChannels.Master] * channels[DefaultSoundChannels.Music];
+            
+            current.Play();
+        }
+        
+        public void Loop(SoundEffect effect)
+        {
+            current?.Dispose();
+
+            current          = effect.CreateInstance();
+            current.Volume   = channels[DefaultSoundChannels.Master] * channels[DefaultSoundChannels.Music];
+            current.IsLooped = true;
+            
+            current.Play();
+        }
+        
+        public bool IsPlaying()
+            => current is { State: SoundState.Playing };
+        
+        public void Stop()
+            => current?.Stop();
+    }
+}
---
+++ Fracture.Engine/Ecs/SoundComponentSystem.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using Fracture.Engine.Events;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Fracture.Engine.Ecs
+{
+    public interface ISoundComponentSystem : IComponentSystem
+    {
+        int Create(int entityId);
+        
+        void Play(int componentId, SoundEffect effect, int listeningEntityId, float maxDistance, float minDistance);
+        void Loop(int componentId, SoundEffect effect, int listeningEntityId, float maxDistance, float minDistance);
+        
+        bool IsPlaying(int componentId);
+        
+        void Stop(int componentId);
+    }
+    
+    public sealed class SoundComponentSystem : ComponentSystem, ISoundComponentSystem
+    {
+        #region Properties
+        
+        #endregion
+        
+        public SoundComponentSystem(IEventQueueSystem events) 
+            : base(events)
+        {
+        }
+
+        public override bool BoundTo(int entityId)
+            => throw new System.NotImplementedException();
+
+        public override int FirstFor(int entityId)
+            => throw new System.NotImplementedException();
+
+        public override IEnumerable<int> AllFor(int entityId)
+            => throw new System.NotImplementedException();
+
+        public int Create(int entityId)
+            => throw new System.NotImplementedException();
+
+        public void Play(int componentId, SoundEffect effect, int listeningEntityId, float maxDistance, float minDistance)
+            => throw new System.NotImplementedException();
+
+        public void Loop(int componentId, SoundEffect effect, int listeningEntityId, float maxDistance, float minDistance)
+            => throw new System.NotImplementedException();
+
+        public bool IsPlaying(int componentId)
+            => throw new System.NotImplementedException();
+
+        public void Stop(int componentId)
+            => throw new System.NotImplementedException();
+    }
+}
